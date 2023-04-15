7c43844d10e975e1a2fa1a5986e78ce767042435
Ryan <ryanmpenrod@gmail.com>
2023-04-15 17:05:36 -0500

    Excessive commenting

--- HackKU23/Assets/Scripts/StalkGrow.cs
+++ HackKU23/Assets/Scripts/StalkGrow.cs
@@ -15,21 +15,27 @@ public class StalkGrow : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // Get mouse position
         mousePosition = UtilsClass.GetMouseWorldPosition();
+
+        // Get direction from object to mouse
         Vector2 direction = new Vector2(
             mousePosition.x - transform.position.x,
             mousePosition.y - transform.position.y
             );
+
+        // Set up direction towards mouse
         transform.up = direction;
 
+        // Initializtions
         localTicks = 0;
         TimeTickSystem.OnTick += TimeTickSystem_OnTick;
-
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Debug for checking direction towards mouse
         Debug.DrawLine(transform.position, mousePosition, Color.green);
     }
 
@@ -37,18 +43,24 @@ public class StalkGrow : MonoBehaviour
     {
         if(localTicks < 1)
         {
+            // Get mouse position
             mousePosition = UtilsClass.GetMouseWorldPosition();
+
+            // Get direction towards mouse
             Vector3 direction = new Vector3(
                 mousePosition.x - transform.position.x,
                 mousePosition.y - transform.position.y,
                 0
             );
+
+            // Generate new position for next segment which is towards mouse
             Vector3 newPos = direction.normalized + transform.position;
 
-            // Create new segment
+            // Create new segment using new position and current rotation
             Instantiate(stalkSegmentPrefab, newPos, transform.rotation);
         }
 
+        // Increment ticks
         localTicks++;
     }
 }
